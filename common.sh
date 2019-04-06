b99fdd637bc72e4862afb6aa29a99da033fb3b64
Jamje Kim <jamje.kim@bespinglobal.com>
2019-04-06 14:16:13 +0900

    split common.sh

---
+++ common.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+OS_NAME="$(uname | awk '{print tolower($0)}')"
+
+L_PAD="  "
+
+command -v fzf > /dev/null && FZF=true
+command -v tput > /dev/null && TPUT=true
+
+_echo() {
+    if [ "${TPUT}" != "" ] && [ "$2" != "" ]; then
+        echo -e "${L_PAD}$(tput setaf $2)$1$(tput sgr0)"
+    else
+        echo -e "${L_PAD}$1"
+    fi
+}
+
+_read() {
+    echo
+    if [ "${TPUT}" != "" ] && [ "$2" != "" ]; then
+        read -p "${L_PAD}$(tput setaf $2)$1$(tput sgr0)" ANSWER
+    else
+        read -p "${L_PAD}$1" ANSWER
+    fi
+}
+
+_success() {
+    echo
+    _echo "+ $@" 2
+    _exit 0
+}
+
+_error() {
+    echo
+    _echo "- $@" 1
+    _exit 1
+}
+
+_exit() {
+    echo
+    exit $1
+}
+
+select_one() {
+    SELECTED=
+
+    CNT=$(cat ${LIST} | wc -l | xargs)
+    if [ "x${CNT}" == "x0" ]; then
+        return
+    fi
+
+    echo
+
+    IDX=0
+    while read VAL; do
+        IDX=$(( ${IDX} + 1 ))
+        printf "%3s. %s\n" "${IDX}" "${VAL}"
+    done < ${LIST}
+
+    if [ "${CNT}" != "1" ]; then
+        CNT="1-${CNT}"
+    fi
+
+    _read "Please select one. (${CNT}) : " 6
+
+    if [ -z ${ANSWER} ]; then
+        return
+    fi
+    TEST='^[0-9]+$'
+    if ! [[ ${ANSWER} =~ ${TEST} ]]; then
+        return
+    fi
+    SELECTED=$(sed -n ${ANSWER}p ${LIST})
+
+}
+
+question() {
+    _read "${1:-"Enter your choice : "}" 6
+
+    if [ ! -z ${2} ]; then
+        if ! [[ ${ANSWER} =~ ${2} ]]; then
+            ANSWER=
+        fi
+    fi
+}
--- run.sh
+++ run.sh
@@ -4,82 +4,16 @@ SHELL_DIR=$(dirname $0)
 
 TEMP_DIR=${SHELL_DIR}/temp
 
+# util func
+source ${SHELL_DIR}/common.sh
+
 prepare() {
-		mkdir -p ${TEMP_DIR}
-}
-
-_echo() {
-    if [ "${TPUT}" != "" ] && [ "$2" != "" ]; then
-        echo -e "${L_PAD}$(tput setaf $2)$1$(tput sgr0)"
-    else
-        echo -e "${L_PAD}$1"
-    fi
-}
-
-_read() {
-    echo
-    if [ "${3}" == "S" ]; then
-        if [ "${TPUT}" != "" ] && [ "$2" != "" ]; then
-            read -s -p "${L_PAD}$(tput setaf $2)$1$(tput sgr0)" ANSWER
-        else
-            read -s -p "${L_PAD}$1" ANSWER
-        fi
-    else
-        if [ "${TPUT}" != "" ] && [ "$2" != "" ]; then
-            read -p "${L_PAD}$(tput setaf $2)$1$(tput sgr0)" ANSWER
-        else
-            read -p "${L_PAD}$1" ANSWER
-        fi
-    fi
-}
-
-_error() {
-    echo
-    _echo "- $@" 1
-    _exit 1
-}
-
-select_one() {
-    OPT=$1
-
-    SELECTED=
-
-    CNT=$(cat ${LIST} | wc -l | xargs)
-    if [ "x${CNT}" == "x0" ]; then
-        return
-    fi
-
-    if [ "${OPT}" != "" ] && [ "x${CNT}" == "x1" ]; then
-        SELECTED="$(cat ${LIST} | xargs)"
-    else
-        echo
-
-        IDX=0
-        while read VAL; do
-            IDX=$(( ${IDX} + 1 ))
-            printf "%3s. %s\n" "${IDX}" "${VAL}"
-        done < ${LIST}
-
-        if [ "${CNT}" != "1" ]; then
-            CNT="1-${CNT}"
-        fi
-
-        _read "Please select one. (${CNT}) : " 6
-
-        if [ -z ${ANSWER} ]; then
-            return
-        fi
-        TEST='^[0-9]+$'
-        if ! [[ ${ANSWER} =~ ${TEST} ]]; then
-            return
-        fi
-        SELECTED=$(sed -n ${ANSWER}p ${LIST})
-    fi
+    mkdir -p ${TEMP_DIR}
 }
 
 get_cluster() {
 
-		LIST=${TEMP_DIR}/cluster-list
+    LIST=${TEMP_DIR}/cluster-list
     kubectl config view -o json | jq -r '.contexts[].name' | sort > ${LIST}
 
     select_one true
@@ -88,16 +22,56 @@ get_cluster() {
         _error
     fi
 
-		CLUSTER_NAME="${SELECTED}"
+    CLUSTER_NAME="${SELECTED}"
 
-		kubectl config use ${CLUSTER_NAME}
+    kubectl config use ${CLUSTER_NAME}
 }
 
 run() {
-		prepare
+    prepare
 
-		get_cluster
+    get_cluster
+
+    main_menu
 }
 
+main_menu() {
+
+    echo
+    _echo "1. istio install"
+    echo
+    _echo "9. istio delete"
+    echo
+    _echo "x. exit"
+
+    question
+
+    case ${ANSWER} in
+        1)
+            istio_install
+            main_menu
+            ;;
+        9)
+            istio_delete
+            main_menu
+            ;;
+        x)
+            _success "Good bye!"
+            ;;
+        *)
+            main_menu
+            ;;
+    esac
+
+}
+
+istio_install() {
+    _echo "istio install"
+
+}
+
+istio_delete() {
+    _echo "istio delete"
+}
 
 run
