3ce220f6c8b639b1972f943afa7978fdbc19c96d
Kim, Byeongmin <claud.kim81@gmail.com>
2019-04-12 18:43:32 +0900

    Update RecommendationResource-time.java

--- file/RecommendationResource-time.java
+++ file/RecommendationResource-time.java
@@ -44,7 +44,7 @@ public class RecommendationResource {
         count++;
         logger.info(String.format("recommendation request from %s: %d", HOSTNAME, count));
 
-        // timeout();
+        timeout();
 
         logger.debug("recommendation service ready to return");
         if (misbehave) {
